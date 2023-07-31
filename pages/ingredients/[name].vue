484505a6a4c0fe6f7b86b680a93343a58919a625
JessicaGosselin <GosselinJessica@hotmail.com>
2023-07-31 13:53:44 -0400

    2 files modified, 1 added

---
+++ pages/ingredients/[name].vue
@@ -0,0 +1,21 @@
+<script setup>
+const route = useRoute();
+
+const name = ref('');
+name.value = route.params.name.toLowerCase();
+
+const { data : ingredient } = await useFetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${name.value}`);
+const test = await useFetch(`https://www.thecocktaildb.com/images/ingredients/${name.value}.png/preview`);
+
+const src = ref('');
+src.value = `https://www.thecocktaildb.com/images/ingredients/${name.value}.png`;
+
+
+</script>
+
+<template>
+	<div>
+		<h1>{{ route.params.name }}</h1>
+		<img :src="src"/>
+	</div>
+</template>
--- pages/ingredients/index.vue
+++ pages/ingredients/index.vue
@@ -1,15 +1,15 @@
 <script setup>
-	const { data: ingredients } = await useFetch(
-	`www.thecocktaildb.com/api/json/v1/1/list.php?i=list`
+	const {data : ingredients }  = await useFetch(
+	`https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list`
 );
 </script>
 
 <template>
 	<div>
-	<h1>Choose your Ingredient</h1>
-		
-	<div class="ingredients">
-		<div v-for="ingredient in ingredients">{{ingredient}}</div>
+		<h1>Choose your Ingredient</h1>
 
+		<div class="ingredients">
+			<NuxtLink :to="`/ingredients/${ingredient.strIngredient1}`" v-for="ingredient in ingredients.drinks">{{ingredient.strIngredient1}}</NuxtLink>
+		</div>
 	</div>
 </template>

b328da8086bfdb2d3baba55d855d7e35b1a2691d
JessicaGosselin <GosselinJessica@hotmail.com>
2023-07-31 13:24:53 -0400

    1 file modified, 3 added

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<nav>
+			<NuxtLink to="/">Home</NuxtLink>
+			<NuxtLink to="/ingredients">Ingredients</NuxtLink>
+		</nav>
+		<slot />
+	</div>
+</template>
+
+<style>
+	nav {
+		display: flex;
+		gap: 15px;
+	}
+</style>
---
+++ pages/ingredients/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+	const { data: ingredients } = await useFetch(
+	`www.thecocktaildb.com/api/json/v1/1/list.php?i=list`
+);
+</script>
+
+<template>
+	<div>
+	<h1>Choose your Ingredient</h1>
+		
+	<div class="ingredients">
+		<div v-for="ingredient in ingredients">{{ingredient}}</div>
+
+	</div>
+</template>

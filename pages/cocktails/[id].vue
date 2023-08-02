0ecf443d4023701a6335ea6cda786fe52561193c
JessicaGosselin <GosselinJessica@hotmail.com>
2023-08-02 11:13:15 -0400

    1 file modified

--- pages/cocktails/[id].vue
+++ pages/cocktails/[id].vue
@@ -1,11 +1,11 @@
 <script setup>
 	const id = useRoute().params.id;
-	const { data : cocktail } = await useFetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-	const drink = ref(cocktail.value.drinks[0]);
+	const { data } = await useFetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+	const cocktail = ref(data.value.drinks[0]);
 	const ingredients = ref([]);
 	for (let i = 1; i <= 15; i++) {
-		let measure = drink.value[`strMeasure${i}`];
-		let ingredient = drink.value[`strIngredient${i}`];
+		let measure = cocktail.value[`strMeasure${i}`];
+		let ingredient = cocktail.value[`strIngredient${i}`];
 		if (ingredient) {
 			ingredients.value.push((measure) ? measure + ' ' + ingredient : ingredient);
 		}
@@ -14,15 +14,15 @@
 
 <template>
 	<div class="single-cocktail">
-		<img :src="drink.strDrinkThumb"/>
+		<img :src="cocktail.strDrinkThumb"/>
 		<div>
-			<h1>{{ drink.strDrink }}</h1>
+			<h1>{{ cocktail.strDrink }}</h1>
 			<h2>You will need:</h2>
 			<ul>
 				<li v-for="ingredient in ingredients">{{ ingredient }}</li>
 			</ul>
 			<h2>Instructions</h2>
-			<div>{{ drink.strInstructions }}</div>
+			<div>{{ cocktail.strInstructions }}</div>
 		</div>
 	</div>
 </template>
